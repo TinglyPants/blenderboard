33cf5779854571e00e995d609f7d03fc8cc82a4e
TinglyPants <chalkytangfastics@gmail.com>
2024-03-25 18:13:00 +0000

    Added searchbar, as well as ability to use a .js file to export SVG paths.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import NavBar from "./components/structure/NavBar";
+import NavBar from "./components/structure/NavBar/NavBar";
+import SearchBar from "./components/structure/NavBar/SearchBar";
 
 export default function App() {
     return (
         <div className="bg-dark w-screen h-screen flex flex-col">
-            <NavBar />
+            <NavBar>
+                <SearchBar />
+            </NavBar>
         </div>
     );
 }
---
+++ src/components/structure/NavBar/SearchBar.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import SvgIcon from "../../general/SvgIcon";
+import { SearchIcon } from "../../SVGPATHS";
+
+export default function SearchBar() {
+    const [searchInput, setSearchInput] = useState("");
+
+    const handleKeyDown = (e) => {
+        if (e.key !== "Enter") {
+            return;
+        }
+        handleSearch(searchInput);
+    };
+
+    const handleSearch = (query) => {
+        console.log("Searched for: " + query);
+        // Add API calls and whatnot here. Do whatever, this is just a placeholder for now.
+    };
+
+    return (
+        <div className="w-full mx-[8rem] p-5 bg-lightest flex items-center rounded-full text-white">
+            <input
+                type="text"
+                className="w-full bg-lightest focus:outline-none"
+                placeholder="Search"
+                onKeyDown={handleKeyDown}
+                onChange={(e) => setSearchInput(e.target.value)}
+            />
+            <SvgIcon
+                onClick={() => handleSearch(searchInput)}
+                name="Search"
+                path={SearchIcon}
+            />
+        </div>
+    );
+}
