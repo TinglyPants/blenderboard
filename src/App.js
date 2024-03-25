f1bbe56f809efccacded55f1ecee04fc57ab00f5
TinglyPants <chalkytangfastics@gmail.com>
2024-03-25 17:29:21 +0000

    Added basic NavBar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import NavBar from "./components/structure/NavBar";
+
 export default function App() {
-    return <></>;
+    return (
+        <div className="bg-dark w-screen h-screen flex flex-col">
+            <NavBar />
+        </div>
+    );
 }
